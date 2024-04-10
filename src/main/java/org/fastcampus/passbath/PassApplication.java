aa2895807c54cc1d43108a08f254d07424d2ff68
younssue <love_rvd7@naver.com>
2024-04-10 18:53:59 +0900

    first commit

--- src/main/java/org/fastcampus/passbath/PassApplication.java
+++ src/main/java/org/fastcampus/passbath/PassApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PassApplication {
 
     public static void main(String[] args) {
+        //주석
         SpringApplication.run(PassApplication.class, args);
     }
 
